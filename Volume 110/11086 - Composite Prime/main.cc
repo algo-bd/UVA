a7dcb044ac096147b17e4f4e0202b5438478ac6c
netcse <smamran09@gmail.com>
2018-03-04 17:09:41 +0600

    Initial Commit

---
+++ Volume 110/11086 - Composite Prime/main.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	cout << "I am a demo cc code" << endl;
+	
+	return 0;
+}
---
+++ demo/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+	
+	printf("I am a demo c code");
+	
+	return 0;
+}
